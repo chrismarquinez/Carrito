9f08ed728ea89583a5770ceeb3fead18d4a6c841
Christian Ruben Marquinez Clevel <cmarquinez@PERNINE.local>
2023-05-29 17:09:19 -0500

    Quinto Commit

--- Backend/controllers/taskController.js
+++ Backend/controllers/taskController.js
@@ -15,11 +15,44 @@
         try {
         const tasks = await Task.find();
         res.status(200).json(tasks);
-    
         } catch (error) {
         res.status(500).json({msg: error.message});
 
         }
     };
 
-    module.exports = {createTask, getTasks, };
+    const  getTask =  async (req,res) => {
+        
+            try {
+                const {id} = req.params
+                const task = await Task.findById(id);
+                if (!task) {
+                    return res.status(404).json(`No Task With Id${id}`);
+                } 
+                res.status(200).json(task);
+            } catch (error) {
+                res.status(500).json({msg: error.message});
+            }
+
+        
+    };
+
+       //Delete Task
+            const deleteTask =  async (req,res) => {
+                try {
+                    const {id} = req.params
+                    const task = await Task.findByIdAndDelete(id)
+                    if (!task) {
+                        return res.status(404).json(`No Task With Id${id}`);
+                    } 
+                    res.status(200).send("Task Delete");
+                } catch (error) {
+                    res.status(500).json({msg: error.message});
+                }
+
+        
+            };
+
+    module.exports = {createTask, getTasks,getTask,deleteTask, };
+
+    
--- Backend/routes/taskRoute.js
+++ Backend/routes/taskRoute.js
@@ -1,11 +1,13 @@
 const express = require ("express");
 const Task = require("../models/taskModel");
-const { createTask, getTasks } = require("../controllers/taskController");
+const { createTask, getTasks , getTask , deleteTask } = require("../controllers/taskController");
 const router = express.Router();
 
 
 router.post("/api/tasks", createTask);
 router.get("/api/tasks", getTasks);
+router.get("/api/tasks/:id", getTask);
+router.get("/api/tasks", deleteTask);
 
 
 module.exports = router;
