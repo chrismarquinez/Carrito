57d07757159369ca6b5f213cbbaad201e39f96e9
Christian Ruben Marquinez Clevel <cmarquinez@PERNINE.local>
2023-06-01 17:06:26 -0500

    Sexto Commit

--- Backend/controllers/taskController.js
+++ Backend/controllers/taskController.js
@@ -40,19 +40,36 @@
        //Delete Task
             const deleteTask =  async (req,res) => {
                 try {
-                    const {id} = req.params
-                    const task = await Task.findByIdAndDelete(id)
+                    const {id} = req.params;
+                    const task =  await Task.findByIdAndDelete(id); 
                     if (!task) {
                         return res.status(404).json(`No Task With Id${id}`);
-                    } 
+                    }   
                     res.status(200).send("Task Delete");
                 } catch (error) {
                     res.status(500).json({msg: error.message});
                 }
-
-        
             };
 
-    module.exports = {createTask, getTasks,getTask,deleteTask, };
+            const updateTask =  async (req,res) => {
+
+                try {
+                const {id} = req.params;
+                const task = await Task.findByIdAndUpdate({ _id: id }, req.body, {
+                    new: true,
+                    runValidators: true,
+                });
+                if (!task) {
+                    return res.status(404).json(`No Task With Id${id}`);
+                }   
+                res.status(200).json(task);
+                } catch (error) {
+                  res.status(500).json({msg: error.message});
+                 }
+            };
+
+            
+
+    module.exports = {createTask, getTasks,getTask,deleteTask, updateTask, };
 
     
